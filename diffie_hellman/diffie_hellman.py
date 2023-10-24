4bc30fce37c4a8da1b393482657cfd68e44fa77c
ashen.blade <htcdnsnnkom2002@gmail.com>
2023-10-24 17:54:53 +0300

    исправил ошибку порядка байтов при передаче

--- diffie_hellman/diffie_hellman.py
+++ diffie_hellman/diffie_hellman.py
@@ -10,14 +10,13 @@ from generator_finder import get_generator
 from utils import power_mod
 
 
-# Число Мерсена (4 байтное, для удобства)
 # https://ru.wikipedia.org/wiki/Список_простых_чисел
-POWER_PRIME = 162259276829213363391578010288127  # 107 бит
+POWER_PRIME = 3253572906719360862808950922543988107309559104397105840021518630897893543395621666798913529440973109  # 107 бит
 
 # Общее число, в которое возводится степень - генератор
 BASE = get_generator(POWER_PRIME)
-
-MSG_SIZE = 16
+print(f'генератор: {BASE}')
+MSG_SIZE = 48
 
 
 class DiffieHellmanClient:
@@ -33,10 +32,10 @@ class DiffieHellmanClient:
 
     def send(self, message: str):
         encoded = encode(message, self.private_key)
-        self.socket.send(encoded, )
+        self.socket.send(encoded)
 
     def receive(self) -> str:
-        received = self.socket.recv(512)
+        received = self.socket.recv(1024)
         return decode(received, self.private_key)
 
     def close(self):
@@ -62,14 +61,14 @@ def accept_client(host: str = '0.0.0.0', port: int = 8080) -> ContextManager[Dif
         sock, addr = server_socket.accept()
         with sock as sock:
             server_socket.close()
-
             logging.debug('Подключился клиент с адреса %s. Читаю его число', addr)
-            client_number = int.from_bytes(sock.recv(MSG_SIZE))
-            logging.debug('Число клиента получено. Отправляю свое')
+            client_number = int.from_bytes(sock.recv(MSG_SIZE), byteorder='big')
+            logging.debug('Число клиента получено: %i', client_number)
             powered = power_mod(BASE, my_power, POWER_PRIME)
             sock.send(powered.to_bytes(MSG_SIZE))
 
             private_key = power_mod(client_number, my_power, POWER_PRIME)
+            logging.info('Рассчитано число: %i', private_key)
             yield DiffieHellmanClient(private_key, sock)
 
 
@@ -86,12 +85,12 @@ def connect_to_server(host: str = '0.0.0.0', port: int = 8080) -> ContextManager
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         logging.debug('Подключаюсь по адресу %s:%i', host, port)
         sock.connect((host, port))
-        logging.debug("Отправляю свой ключ на сервер. Ключ: %i", my_power)
         powered = power_mod(BASE, my_power, POWER_PRIME)
+        logging.debug("Отправляю свой ключ на сервер. Ключ: %i", powered)
         sock.send(powered.to_bytes(MSG_SIZE))
-        logging.debug('Получаю ключ сервера')
         data = sock.recv(MSG_SIZE)
-        server_number = int.from_bytes(data)
+        server_number = int.from_bytes(data, byteorder='big')
+        logging.debug('Ключ сервера получен: %i', server_number)
         private_key = power_mod(server_number, my_power, POWER_PRIME)
         logging.debug('Рассчитанный ключ %i', private_key)
         yield DiffieHellmanClient(private_key, sock)
@@ -111,5 +110,6 @@ def decode(data: bytes, key: list[int]) -> str:
         d ^ k
         for d, k in zip(data, cycle(key))
     ]
-    return bytes(result).decode()
+    b = bytes(result)
+    return b.decode()
 
